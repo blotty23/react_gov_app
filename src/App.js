4e02b469a9dfca80b9bb33271769b8a97e0c27ca
Emma Blott <blott23@gmail.com>
2020-11-14 18:57:49 +0000

    write topNavHeader component that changes colour depending on what current page is

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
+import { useState } from "react";
+
+import TopNavHeader from './components/TopNavHeader'
+
+const adminTopLinks = [
+  {
+    id:"home",
+    name: "Home", 
+    link: "/"
+  },
+  {
+    id:"myProfile",
+    name: "My Profile", 
+    link: "/profile"
+  },
+  {
+    id:"myEmployees",
+    name: "My Employees", 
+    link: "/employees"
+  },
+  {
+    id:"visitors",
+    name: "Visitors", 
+    link: "/visitors"
+  }
+]
 
 function App() {
+  const [linksList, setLinksList] = useState(adminTopLinks)
+  const [userType, setUserType] = useState("admin")
+  const [currentPage, setCurrentPage] = useState("myEmployees")
+
+
+
   return (
-  <div>hello world</div>
+    <TopNavHeader 
+      currentPage={currentPage}
+      linksList={linksList} 
+    />
   );
 }
 
--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,9 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from '../App';
-import '@testing-library/jest-dom';
+import '@testing-library/jest-dom'
+import React from 'react'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/hello world/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
